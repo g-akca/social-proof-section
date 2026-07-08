810acdbb06d22cdb493f329fe53730de62aa72a9
g-akca <gok.akca66@gmail.com>
2026-07-08 18:08:43 +0300

    Give App styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import TestimonialsList from "./components/TestimonialsList";
 
 function App() {
   return (
-    <div>
+    <div className="text-base leading-base px-6 py-20 flex flex-col gap-12 text-purple-900">
       <HeroSection />
 
       <TestimonialsList />
