fe06480e4656372d683fae6c1c33c5eaf84f2d19
g-akca <gok.akca66@gmail.com>
2026-07-08 22:49:02 +0300

    Add starCount and reviewer props to ReviewItem component, add content and styles

--- src/components/ReviewItem.jsx
+++ src/components/ReviewItem.jsx
@@ -1,7 +1,19 @@
-function ReviewItem() {
+function ReviewItem({ starCount, reviewer }) {
+  const stars = Array.from({ length: starCount }, (_, index) => (
+    <img
+      key={index}
+      src="/images/icon-star.svg"
+      alt=""
+      className="w-4 h-4"
+    />
+  ))
+
   return (
-    <>
-    </>
+    <div className="bg-purple-100 rounded-lg p-3.75 flex flex-col gap-4 items-center">
+      <div className="flex gap-2">{stars}</div>
+
+      <p className="text-[17px] leading-[100%] font-bold">Rated {starCount} stars in {reviewer}</p>
+    </div>
   )
 }
 
