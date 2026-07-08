640cf6d62e5a6d65c69ac06c3c43a346910857f1
g-akca <gok.akca66@gmail.com>
2026-07-08 19:25:19 +0300

    Add HeaderSection content and styles

--- src/components/HeaderSection.jsx
+++ src/components/HeaderSection.jsx
@@ -1,7 +1,10 @@
 function HeaderSection() {
   return (
-    <>
-    </>
+    <div className="flex flex-col gap-4 text-center">
+      <h1 className="text-[40px] leading-[85%] font-bold tracking-[-2px] px-4">10,000+ of our users love our products.</h1>
+
+      <p className="text-purple-300 font-medium tracking-[-0.6px]">We only provide great products combined with excellent customer service. See what our satisfied customers are saying about our services.</p>
+    </div>
   )
 }
 
