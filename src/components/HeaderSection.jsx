094937d0609a54bfe8073d361691c535f1733430
g-akca <gok.akca66@gmail.com>
2026-07-09 22:52:24 +0300

    Add desktop responsive HeroSection and HeaderSection styles

--- src/components/HeaderSection.jsx
+++ src/components/HeaderSection.jsx
@@ -1,7 +1,7 @@
 function HeaderSection() {
   return (
-    <div className="flex flex-col items-center gap-4 text-center w-full max-w-146 desktop:max-w-none">
-      <h1 className="text-[40px] leading-[85%] font-bold tracking-[-2px] px-4 tablet:text-[56px]">10,000+ of our users love our products.</h1>
+    <div className="flex flex-col items-center gap-4 text-center w-full max-w-146 desktop:max-w-111.25 desktop:items-start desktop:text-start">
+      <h1 className="text-[40px] leading-[85%] font-bold tracking-[-2px] px-4 tablet:text-[56px] desktop:px-0">10,000+ of our users love our products.</h1>
 
       <p className="text-purple-300 font-medium tracking-[-0.6px] max-w-113.25 desktop:max-w-none">We only provide great products combined with excellent customer service. See what our satisfied customers are saying about our services.</p>
     </div>
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,7 +3,7 @@ import ReviewsList from "./ReviewsList";
 
 function HeroSection() {
   return (
-    <div className="flex flex-col items-center gap-8 tablet:gap-10 w-full desktop:max-w-277.5">
+    <div className="flex flex-col items-center gap-8 tablet:gap-10 w-full desktop:max-w-277.5 desktop:flex-row desktop:justify-between">
       <HeaderSection />
 
       <ReviewsList />
