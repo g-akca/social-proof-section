ee1a0639167d9516d4ba0bf001e9a14ef984b141
g-akca <gok.akca66@gmail.com>
2026-07-09 21:34:25 +0300

    Add tablet responsive gap adjustment to HeroSection

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,7 +3,7 @@ import ReviewsList from "./ReviewsList";
 
 function HeroSection() {
   return (
-    <div className="flex flex-col gap-8 w-full max-w-113.25">
+    <div className="flex flex-col gap-8 w-full max-w-113.25 tablet:gap-10">
       <HeaderSection />
 
       <ReviewsList />
