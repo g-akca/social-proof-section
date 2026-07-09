946591bdbb53c45f3fa8728f3d44000f4538d0ee
g-akca <gok.akca66@gmail.com>
2026-07-09 19:45:37 +0300

    Add image properties to testimonials in TestimonialsList

--- src/components/TestimonialsList.jsx
+++ src/components/TestimonialsList.jsx
@@ -4,31 +4,35 @@ const testimonials = [
   {
     name: "Colton Smith",
     title: "Verified Buyer",
-    message: "We needed the same printed design as the one we had ordered a week prior. Not only did they find the original order, but we also received it in time. Excellent!"
+    message: "We needed the same printed design as the one we had ordered a week prior. Not only did they find the original order, but we also received it in time. Excellent!",
+    image: "/images/image-colton.jpg"
   },
   {
     name: "Irene Roberts",
     title: "Verified Buyer",
-    message: "Customer service is always excellent and very quick turn around. Completely delighted with the simplicity of the purchase and the speed of delivery."
+    message: "Customer service is always excellent and very quick turn around. Completely delighted with the simplicity of the purchase and the speed of delivery.",
+    image: "/images/image-irene.jpg"
   },
   {
     name: "Anne Wallace",
     title: "Verified Buyer",
-    message: "Put an order with this company and can only praise them for the very high standard. Will definitely use them again and recommend them to everyone!"
+    message: "Put an order with this company and can only praise them for the very high standard. Will definitely use them again and recommend them to everyone!",
+    image: "/images/image-anne.jpg"
   },
 ];
 
 function TestimonialsList() {
   return (
     <div className="flex flex-col gap-4">
-      {testimonials.map(tst => {
+      {testimonials.map(tst => (
         <TestimonialItem
           key={tst.name}
           name={tst.name}
           title={tst.title}
           message={tst.message}
+          image={tst.image}
         />
-      })}
+      ))}
     </div>
   )
 }
