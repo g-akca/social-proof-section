2c1a3f6608d5e0773b5cb550c7d153b96417587a
g-akca <gok.akca66@gmail.com>
2026-07-09 21:13:02 +0300

    Update image paths in TestimonialsList to use base URL

--- src/components/TestimonialsList.jsx
+++ src/components/TestimonialsList.jsx
@@ -1,23 +1,25 @@
 import TestimonialItem from "./TestimonialItem";
 
+const baseUrl = import.meta.env.BASE_URL
+
 const testimonials = [
   {
     name: "Colton Smith",
     title: "Verified Buyer",
     message: "We needed the same printed design as the one we had ordered a week prior. Not only did they find the original order, but we also received it in time. Excellent!",
-    image: "/images/image-colton.jpg"
+    image: `${baseUrl}images/image-colton.jpg`
   },
   {
     name: "Irene Roberts",
     title: "Verified Buyer",
     message: "Customer service is always excellent and very quick turn around. Completely delighted with the simplicity of the purchase and the speed of delivery.",
-    image: "/images/image-irene.jpg"
+    image: `${baseUrl}images/image-irene.jpg`
   },
   {
     name: "Anne Wallace",
     title: "Verified Buyer",
     message: "Put an order with this company and can only praise them for the very high standard. Will definitely use them again and recommend them to everyone!",
-    image: "/images/image-anne.jpg"
+    image: `${baseUrl}images/image-anne.jpg`
   },
 ];
 
