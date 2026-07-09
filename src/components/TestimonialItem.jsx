b645c937e83b0f613876bebaabd7a950f5236ea4
g-akca <gok.akca66@gmail.com>
2026-07-09 21:12:51 +0300

    Give TestimonialItem content and styles

--- src/components/TestimonialItem.jsx
+++ src/components/TestimonialItem.jsx
@@ -1,7 +1,17 @@
-function TestimonialItem({ name, title, message }) {
+function TestimonialItem({ name, title, message, image }) {
   return (
-    <>
-    </>
+    <div className="bg-purple-900 rounded-lg p-8 text-white text-[17px] flex flex-col gap-8">
+      <div className="flex gap-6 items-center">
+        <img src={image} alt={name} className="w-10 aspect-square rounded-full" />
+
+        <div className="leading-[100%]">
+          <p className="font-bold">{name}</p>
+          <p className="text-pink-400">{title}</p>
+        </div>
+      </div>
+
+      <p className="leading-[130%] font-medium">“ {message} ”</p>
+    </div>
   )
 }
 
