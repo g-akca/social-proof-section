37ed0aa4ee31d6d8b7febaf6e624e196f894cc83
g-akca <gok.akca66@gmail.com>
2026-07-09 22:14:24 +0300

    Add tablet responsive ReviewItem styles

--- src/components/ReviewItem.jsx
+++ src/components/ReviewItem.jsx
@@ -11,10 +11,10 @@ function ReviewItem({ starCount, reviewer }) {
   ))
 
   return (
-    <div className="bg-purple-100 rounded-lg p-3.75 flex flex-col gap-4 items-center">
+    <div className="bg-purple-100 rounded-lg p-3.75 flex flex-col gap-4 items-center tablet:px-8 tablet:py-4 tablet:flex-row tablet:gap-8">
       <div className="flex gap-2">{stars}</div>
 
-      <p className="text-[17px] leading-[100%] font-bold">Rated {starCount} stars in {reviewer}</p>
+      <p className="text-[17px] leading-[100%] font-bold">Rated {starCount} Stars in {reviewer}</p>
     </div>
   )
 }
