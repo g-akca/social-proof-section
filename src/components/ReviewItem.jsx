aafed88337b2c403df9ea5adcf24602ccf57a26e
g-akca <gok.akca66@gmail.com>
2026-07-10 09:52:50 +0300

    Add className prop to ReviewItem and update ReviewsList with className for responsive layout

--- src/components/ReviewItem.jsx
+++ src/components/ReviewItem.jsx
@@ -1,6 +1,6 @@
 import starIcon from "/images/icon-star.svg";
 
-function ReviewItem({ starCount, reviewer }) {
+function ReviewItem({ starCount, reviewer, className }) {
   const stars = Array.from({ length: starCount }, (_, index) => (
     <img
       key={index}
@@ -11,7 +11,7 @@ function ReviewItem({ starCount, reviewer }) {
   ))
 
   return (
-    <div className="bg-purple-100 rounded-lg p-3.75 flex flex-col gap-4 items-center tablet:px-8 tablet:py-4 tablet:flex-row tablet:gap-8">
+    <div className={`bg-purple-100 rounded-lg p-3.75 flex flex-col gap-4 items-center tablet:px-8 tablet:py-4 tablet:flex-row tablet:gap-8 desktop:w-111.5 ${className}`}>
       <div className="flex gap-2">{stars}</div>
 
       <p className="text-[17px] leading-[100%] font-bold">Rated {starCount} Stars in {reviewer}</p>
--- src/components/ReviewsList.jsx
+++ src/components/ReviewsList.jsx
@@ -3,26 +3,30 @@ import ReviewItem from "./ReviewItem";
 const reviews = [
   {
     starCount: 5,
-    reviewer: "Reviews"
+    reviewer: "Reviews",
+    className: "self-start"
   },
   {
     starCount: 5,
-    reviewer: "Report Guru"
+    reviewer: "Report Guru",
+    className: "self-center"
   },
   {
     starCount: 5,
-    reviewer: "BestTech"
+    reviewer: "BestTech",
+    className: "self-end"
   },
 ];
 
 function ReviewsList() {
   return (
-    <div className="flex flex-col gap-4 w-full max-w-113.25 desktop:max-w-none">
+    <div className="flex flex-col gap-4 w-full max-w-113.25 desktop:max-w-none desktop:w-135.5">
       {reviews.map(review => (
         <ReviewItem
           key={review.reviewer}
           starCount={review.starCount}
           reviewer={review.reviewer}
+          className={review.className}
         />
       ))}
     </div>
